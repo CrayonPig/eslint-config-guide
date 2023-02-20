f1c2cfc0368084e8ec1a9bfcd0cb88df39e64fe1
CrayonPig <475003925@qq.com>
2023-02-20 15:01:09 +0800

    feat: 调整代码架构

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  configs: {
+    base: require('./lib/configs/base'),
+    react: require('./lib/configs/react'),
+    reactHooks: require('./lib/configs/react-hooks'),
+  },
+};
